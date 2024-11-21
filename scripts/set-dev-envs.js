f170841b9971f09a12693c607b220416bd0d2b7d
Carles Idealive <carles.labrana@idealiveconsulting.com>
2024-11-21 10:41:03 +0100

    CONSTRUCCION ENVIRONMENTS

--- scripts/set-dev-envs.js
+++ scripts/set-dev-envs.js
@@ -3,14 +3,22 @@ const { writeFileSync, mkdirSync } = require('fs');
 require('dotenv').config();
 
 // Generate environment.ts file based on the environment variables
-const targetPath = `./src/environments/environment.development.ts`;
-const envFileContent = `
+const targetPath = `./src/environments`;
+const envFileContent_Development = `
 export const environment = {
   production: false,
   syncFusionLicense: "${process.env['SYNCFUSION_LICENSE']}",
 };
 `;
+const envFileContent = `
+export const environment = {
+  production: true  ,
+  syncFusionLicense: "",
+};
+`;
+
 
 //Crea el directorio
 mkdirSync('./src/environments', { recursive: true  });
-writeFileSync(targetPath, envFileContent);
+writeFileSync(`${targetPath}/environment.development.ts`, envFileContent_Development);
+writeFileSync(`${targetPath}/environment.ts`, envFileContent);
